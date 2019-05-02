b9a750c73fe403df6733aade46aa9de606b7265f
mark <mejiomah17>
2019-05-02 12:55:53 +0300

    поменял типы данных

--- src/main/java/com/github/mejiomah17/fns/api/Receipt.java
+++ src/main/java/com/github/mejiomah17/fns/api/Receipt.java
@@ -9,7 +9,7 @@ public class Receipt {
 
     @SerializedName("ecashTotalSum")
     @Expose
-    private int ecashTotalSum;
+    private long ecashTotalSum;
     @SerializedName("userInn")
     @Expose
     private String userInn;
@@ -18,22 +18,22 @@ public class Receipt {
     private List<Item> items = null;
     @SerializedName("requestNumber")
     @Expose
-    private int requestNumber;
+    private long requestNumber;
     @SerializedName("provisionSum")
     @Expose
-    private int provisionSum;
+    private long provisionSum;
     @SerializedName("fiscalSign")
     @Expose
-    private int fiscalSign;
+    private long fiscalSign;
     @SerializedName("fiscalDocumentNumber")
     @Expose
-    private int fiscalDocumentNumber;
+    private long fiscalDocumentNumber;
     @SerializedName("operationType")
     @Expose
-    private int operationType;
+    private long operationType;
     @SerializedName("taxationType")
     @Expose
-    private int taxationType;
+    private long taxationType;
     @SerializedName("messageFiscalSign")
     @Expose
     private long messageFiscalSign;
@@ -45,13 +45,13 @@ public class Receipt {
     private String machineNumber;
     @SerializedName("cashTotalSum")
     @Expose
-    private int cashTotalSum;
-    @SerializedName("internetSign")
+    private long cashTotalSum;
+    @SerializedName("longernetSign")
     @Expose
-    private int internetSign;
+    private long longernetSign;
     @SerializedName("shiftNumber")
     @Expose
-    private int shiftNumber;
+    private long shiftNumber;
     @SerializedName("operator")
     @Expose
     private String operator;
@@ -63,22 +63,22 @@ public class Receipt {
     private String dateTime;
     @SerializedName("ndsNo")
     @Expose
-    private int ndsNo;
+    private long ndsNo;
     @SerializedName("totalSum")
     @Expose
-    private int totalSum;
+    private long totalSum;
     @SerializedName("fiscalDocumentFormatVer")
     @Expose
-    private int fiscalDocumentFormatVer;
+    private long fiscalDocumentFormatVer;
     @SerializedName("rawData")
     @Expose
     private String rawData;
     @SerializedName("prepaidSum")
     @Expose
-    private int prepaidSum;
+    private long prepaidSum;
     @SerializedName("paymentAgentType")
     @Expose
-    private int paymentAgentType;
+    private long paymentAgentType;
     @SerializedName("sellerAddress")
     @Expose
     private String sellerAddress;
@@ -93,10 +93,10 @@ public class Receipt {
     private String user;
     @SerializedName("receiptCode")
     @Expose
-    private int receiptCode;
+    private long receiptCode;
     @SerializedName("creditSum")
     @Expose
-    private int creditSum;
+    private long creditSum;
     @SerializedName("kktRegId")
     @Expose
     private String kktRegId;
@@ -104,11 +104,11 @@ public class Receipt {
     @Expose
     private String buyerAddress;
 
-    public int getEcashTotalSum() {
+    public long getEcashTotalSum() {
         return ecashTotalSum;
     }
 
-    public void setEcashTotalSum(int ecashTotalSum) {
+    public void setEcashTotalSum(long ecashTotalSum) {
         this.ecashTotalSum = ecashTotalSum;
     }
 
@@ -128,51 +128,51 @@ public class Receipt {
         this.items = items;
     }
 
-    public int getRequestNumber() {
+    public long getRequestNumber() {
         return requestNumber;
     }
 
-    public void setRequestNumber(int requestNumber) {
+    public void setRequestNumber(long requestNumber) {
         this.requestNumber = requestNumber;
     }
 
-    public int getProvisionSum() {
+    public long getProvisionSum() {
         return provisionSum;
     }
 
-    public void setProvisionSum(int provisionSum) {
+    public void setProvisionSum(long provisionSum) {
         this.provisionSum = provisionSum;
     }
 
-    public int getFiscalSign() {
+    public long getFiscalSign() {
         return fiscalSign;
     }
 
-    public void setFiscalSign(int fiscalSign) {
+    public void setFiscalSign(long fiscalSign) {
         this.fiscalSign = fiscalSign;
     }
 
-    public int getFiscalDocumentNumber() {
+    public long getFiscalDocumentNumber() {
         return fiscalDocumentNumber;
     }
 
-    public void setFiscalDocumentNumber(int fiscalDocumentNumber) {
+    public void setFiscalDocumentNumber(long fiscalDocumentNumber) {
         this.fiscalDocumentNumber = fiscalDocumentNumber;
     }
 
-    public int getOperationType() {
+    public long getOperationType() {
         return operationType;
     }
 
-    public void setOperationType(int operationType) {
+    public void setOperationType(long operationType) {
         this.operationType = operationType;
     }
 
-    public int getTaxationType() {
+    public long getTaxationType() {
         return taxationType;
     }
 
-    public void setTaxationType(int taxationType) {
+    public void setTaxationType(long taxationType) {
         this.taxationType = taxationType;
     }
 
@@ -200,27 +200,27 @@ public class Receipt {
         this.machineNumber = machineNumber;
     }
 
-    public int getCashTotalSum() {
+    public long getCashTotalSum() {
         return cashTotalSum;
     }
 
-    public void setCashTotalSum(int cashTotalSum) {
+    public void setCashTotalSum(long cashTotalSum) {
         this.cashTotalSum = cashTotalSum;
     }
 
-    public int getInternetSign() {
-        return internetSign;
+    public long getlongernetSign() {
+        return longernetSign;
     }
 
-    public void setInternetSign(int internetSign) {
-        this.internetSign = internetSign;
+    public void setlongernetSign(long longernetSign) {
+        this.longernetSign = longernetSign;
     }
 
-    public int getShiftNumber() {
+    public long getShiftNumber() {
         return shiftNumber;
     }
 
-    public void setShiftNumber(int shiftNumber) {
+    public void setShiftNumber(long shiftNumber) {
         this.shiftNumber = shiftNumber;
     }
 
@@ -248,27 +248,27 @@ public class Receipt {
         this.dateTime = dateTime;
     }
 
-    public int getNdsNo() {
+    public long getNdsNo() {
         return ndsNo;
     }
 
-    public void setNdsNo(int ndsNo) {
+    public void setNdsNo(long ndsNo) {
         this.ndsNo = ndsNo;
     }
 
-    public int getTotalSum() {
+    public long getTotalSum() {
         return totalSum;
     }
 
-    public void setTotalSum(int totalSum) {
+    public void setTotalSum(long totalSum) {
         this.totalSum = totalSum;
     }
 
-    public int getFiscalDocumentFormatVer() {
+    public long getFiscalDocumentFormatVer() {
         return fiscalDocumentFormatVer;
     }
 
-    public void setFiscalDocumentFormatVer(int fiscalDocumentFormatVer) {
+    public void setFiscalDocumentFormatVer(long fiscalDocumentFormatVer) {
         this.fiscalDocumentFormatVer = fiscalDocumentFormatVer;
     }
 
@@ -280,19 +280,19 @@ public class Receipt {
         this.rawData = rawData;
     }
 
-    public int getPrepaidSum() {
+    public long getPrepaidSum() {
         return prepaidSum;
     }
 
-    public void setPrepaidSum(int prepaidSum) {
+    public void setPrepaidSum(long prepaidSum) {
         this.prepaidSum = prepaidSum;
     }
 
-    public int getPaymentAgentType() {
+    public long getPaymentAgentType() {
         return paymentAgentType;
     }
 
-    public void setPaymentAgentType(int paymentAgentType) {
+    public void setPaymentAgentType(long paymentAgentType) {
         this.paymentAgentType = paymentAgentType;
     }
 
@@ -328,19 +328,19 @@ public class Receipt {
         this.user = user;
     }
 
-    public int getReceiptCode() {
+    public long getReceiptCode() {
         return receiptCode;
     }
 
-    public void setReceiptCode(int receiptCode) {
+    public void setReceiptCode(long receiptCode) {
         this.receiptCode = receiptCode;
     }
 
-    public int getCreditSum() {
+    public long getCreditSum() {
         return creditSum;
     }
 
-    public void setCreditSum(int creditSum) {
+    public void setCreditSum(long creditSum) {
         this.creditSum = creditSum;
     }
 
